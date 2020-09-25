afc92c6c2781c845e5c0f9431a953ee542a19d23
Confidence Okoghenun <confidenceboi@gmail.com>
2020-09-25 15:22:02 +0100

    feat: Adds landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/**  @jsx jsx  */
+import { jsx } from '@emotion/core';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import Home from './routes/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' css={{ fontFamily: '\'Noto Sans JP\', sans-serif !Important' }}>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/routes/Home.js
@@ -0,0 +1,96 @@
+/**  @jsx jsx  */
+import { jsx } from '@emotion/core';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <div
+        css={{
+          width: '100vw',
+          height: '100vh',
+          filter: 'blur(5px)',
+          position: 'absolute',
+          backgroundImage: 'url("/image/mad_bus.jpg")',
+        }}
+      ></div>
+      <nav></nav>
+      <div
+        css={{
+          top: '50%',
+          left: '50%',
+          color: '#FFF',
+          position: 'fixed',
+          transform: 'translate(-50%, -50%)',
+        }}
+      >
+        <header>
+          <div
+            css={{
+              display: 'flex',
+              alignItems: 'center',
+              flexDirection: 'column',
+            }}
+          >
+            <img
+              css={{ width: '50px' }}
+              src='/image/boromi_dark.png'
+              alt='boromi'
+            />
+            <span
+              css={{
+                fontSize: '2em',
+                textShadow: '1px 1px 2px #000',
+              }}
+            >
+              boromi
+            </span>
+          </div>
+        </header>
+        <section css={{ textAlign: 'center' }}>
+          <div
+            css={{
+              display: 'flex',
+              margin: '30px 0',
+              justifyContent: 'center',
+              textShadow: '1px 1px 2px #000',
+            }}
+          >
+            <p css={{ maxWidth: '500px' }}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
+              delectus, dolores odio possimus quis alias ab, blanditiis aperiam
+              magni explicabo recusandae nobis magnam beatae! In autem odio
+              quaerat magni facilis.
+            </p>
+          </div>
+          <Link
+            css={{
+              color: '#FFF',
+              width: '170px',
+              height: '46px',
+              fontWeight: 500,
+              fontSize: '1.2em',
+              alignItems: 'center',
+              borderRadius: '50px',
+              background: '#3C007F',
+              display: 'inline-flex',
+              textDecoration: 'none',
+              justifyContent: 'center',
+              textTransform: 'capitalize',
+              border: '2px #3C007F solid',
+              ':hover': {
+                color: '#3C007F',
+                background: '#FFF',
+              },
+            }}
+            to='/signup'
+          >
+            <span>get started</span>
+          </Link>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
